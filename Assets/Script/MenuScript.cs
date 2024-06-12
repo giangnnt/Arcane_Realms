5c7c06dacabc518a64a6125e6443b5f425ed6a91
Nguyen Vu <KanaeRudo@gmail.com>
2024-06-12 14:56:13 +0700

    Tiles-fix & menu & sample scene

---
+++ Assets/Script/MenuScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Option()
+    {
+
+    }
+
+    public void Load()
+    {
+
+    }
+}
