2d52014c0970232bab500fafa4376828af87f359
@giangnntse173469 <giangnntse173469@fpt.edu.vn>
2024-07-05 19:21:05 +0700

    a

--- Assets/Script/CameraSingleton.cs
+++ Assets/Script/CameraSingleton.cs
@@ -1,4 +1,18 @@
 public class CameraSingleton : Singleton<CameraSingleton>
 {
+    private static CameraSingleton instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
 
